8953da5165788d654c1eb58f5cd6f78b9631e23c
Bruce Dickie <38924242+BruceMD@users.noreply.github.com>
2020-11-10 16:47:19 +0900

    Commit 2 - convert to proper Roman Numerals

--- Problem89.py
+++ Problem89.py
@@ -1,21 +1,53 @@
 def main():
 
+	# Commit 1
 	# quick roman numerals to decimal system converter
 	# not over the moon with the open() function that stores all my data in my code into lis
 	# then processes all the data, would be smarter to handle each line from 'p089_roman.txt'
 	# instead of saving each file but this is only 1000 lines long so not the end of the world
 
+	genDecimals()
+	
+	# Commit 2
+	
+	genRoman()
+	
+	
+def genRoman():
+	lis = []
+	with open("RomanToDecimals.txt", "r") as file:
+		for item in file.readlines():
+			lis.append(item.strip("\n"))
+#	print(lis)
+	
+	libRef = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+	lib = {1000: "M", 900: "CM", 500:"D", 400:"CD", 100:"C", 90:"XC", 50:"L", 40:"XL", 10:"X", 9:"IX", 5:"V", 4:"IV", 1:"I"}
+	
+	newFile = open("DecimalToRoman.txt", "w")
+	
+	for number in lis:
+		n = 0
+		decimalNum = int(number)
+		romanNum = ""
+		while decimalNum > 0:
+			if decimalNum >= libRef[n]:
+				decimalNum -= libRef[n]
+				romanNum += lib[libRef[n]]
+			else:
+				n += 1
+		newFile.write(romanNum + "\n")
 
+	newFile.close()
+
+def genDecimals():
 	lis = []
 	with open('p089_roman.txt', 'r') as file:
-		n = 1
 		for item in file.readlines():
-			n += 1
 			lis.append(item.strip("\n"))
 			
-	print(lis)
+#	print(lis)
 	
-	newFile = open("resultsFile.txt", "w")
+	newFile = open("RomanToDecimals.txt", "w")
 	
 	romanN = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
 	
@@ -32,9 +64,9 @@ def main():
 			else:
 				podium = romanN[digit] - podium
 		numberValue += podium
-		newFile.write(number + " >>> " + str(numberValue) + "\n")
+		newFile.write(str(numberValue) + "\n")
 	
-	
+	print(count)
 	newFile.close()
 	
 def check(a, b):
