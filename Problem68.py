c28264fccb877232e3dc16609b06cca04de8beee
Bruce Dickie <38924242+BruceMD@users.noreply.github.com>
2020-11-12 18:54:14 +0900

    Final update
    Comments added for the future

--- Problem68.py
+++ Problem68.py
@@ -8,23 +8,23 @@ def main():
 	
 	for lim in range(14, 20):
 		for perm in permutations(lis, 5):
-# I found the permutation of five numbers that work on a Gon ring, then matched the remaining five to where they could go. If there was a conflict then I'd quit this perm and move to the next option
+									# Explore all permutations of 5 numbers that make up the inner core -> perm(1:10, 5)
 			check = True
 			invPerm = invLis(perm)
 			for j in range(-1, 4):
 				num = lim - perm[j] - perm[j+1]
-				if num in invPerm:
+				if num in invPerm:			# Then matched the remaining five to where they could go
 					invPerm.remove(num)
 				else:
-					check = False
+					check = False			# If there was a conflict then I'd quit this perm and move to the next option
 					break
-			if check:
+			if check:					# No conflicts, then send to printGonRing to order them correctly
 				validGong.append(printGonRing(perm, lim))
 			
 	print(findMax(restrict(validGong)))
 
 	
-def restrict(rings):
+def restrict(rings):						# only allow cases where 10 is in the external nodes (index 0:4)
 	newRingList = []
 	
 	for ring in rings:
@@ -33,7 +33,7 @@ def restrict(rings):
 	return newRingList
 
 	
-def findMax(rings):
+def findMax(rings):							# Go through each ring and see which is the largest
 	tempMax = 0
 	
 	for ring in rings:
@@ -48,23 +48,24 @@ def localMax(gonRing):
 	# need to keep cyclical order important within each side, but because it is cyclical, we can choose where to start
 	side1, side2, side3, side4, side5 = [0, 6, 7], [1, 7, 8], [2, 8, 9], [3, 9, 5], [4, 5, 6]
 	gonRingColl = [side1, side2, side3, side4, side5]
-
+								# list of lists because sequence is not important to a dictionary
 
 	i = startInd(gonRing)					# starting from the lowest external node
 
 	tempNum = ""
-	for j in fivePerm(i):					# go through each side
+	for j in fivePerm(i):					# go through each side starting at i
 		for k in gonRingColl[j]:			# add each number from each side
-			tempNum += str(gonRing[k])
+			tempNum += str(gonRing[k])		# there seemed to be something wrong with tempNum += tempNum*10 + gonRIng[k]
+								# so I converted to a string, put them together and converted back to a integer
 
 	return int(tempNum)
 
 
-def startInd(ring):
+def startInd(ring):						# choose min from the first 5 numbers - external nodes
 	return ring.index(min(ring[0:5]))
 
 
-def fivePerm(x):
+def fivePerm(x):						# find the index of starting min external node and travel in a circle from here
 	fiveLis = []
 	for h in range(x, x+5):
 		if h > 4:
@@ -90,7 +91,7 @@ def printGonRing(perm, lim):
 	return lis
 
 
-def invLis(p):
+def invLis(p):								# whatever is in p will not be in invLis, pretty self explanatory
 	invLis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 	for n in p:
 		if n in invLis:
